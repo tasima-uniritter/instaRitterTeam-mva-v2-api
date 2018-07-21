e609fc7948c19804b899852f7b37302d2718a075
Allan Moreira Leite <allanmleite@hotmail.com>
2018-07-21 17:42:46 -0300

    repositorio inicializado

---
+++ src/main/java/com/uniritter/instaRitterTeam/instaRitterTeammvav2api/InstaRitterTeamMvaV2ApiApplication.java
@@ -0,0 +1,12 @@
+package com.uniritter.instaRitterTeam.instaRitterTeammvav2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaRitterTeamMvaV2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaRitterTeamMvaV2ApiApplication.class, args);
+	}
+}
