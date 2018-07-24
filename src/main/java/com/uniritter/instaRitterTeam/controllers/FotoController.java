538eb6793a88e45751575d75ab6501746d0a80ef
Allan Moreira Leite <allanmleite@hotmail.com>
2018-07-24 00:15:28 -0300

    endpoint incluir e consultar, configuracao cors

---
+++ src/main/java/com/uniritter/instaRitterTeam/controllers/FotoController.java
@@ -0,0 +1,31 @@
+package com.uniritter.instaRitterTeam.controllers;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uniritter.instaRitterTeam.dtos.FotoParaIncluirDto;
+import com.uniritter.instaRitterTeam.models.Foto;
+import com.uniritter.instaRitterTeam.services.FotoService;
+
+@RestController
+public class FotoController {
+
+	@Autowired
+	private FotoService service;
+
+	@RequestMapping(value = "/foto", method = RequestMethod.POST)
+	public Foto incluir(@RequestBody FotoParaIncluirDto fotoParaIncluirDto) {
+		return service.incluir(fotoParaIncluirDto);
+	}
+
+	@RequestMapping(value = "/foto/{id}", method = RequestMethod.GET)
+	public Foto consultar(@PathVariable("id") Long id) {
+		return service.consultar(id);
+	}
+}
