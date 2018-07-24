f36ed72ff011f64b9929f9f3f9b065c3911bd7e0
Allan Moreira Leite <allan.leite@srmasset.com>
2018-07-24 13:44:54 -0300

    notacao @transactional para dar rollback antes de cada teste

---
+++ src/test/java/com/uniritter/instaRitterTeam/BaseTeste.java
@@ -0,0 +1,23 @@
+package com.uniritter.instaRitterTeam;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource("classpath:test.properties")
+@Transactional
+public class BaseTeste {
+
+	@Test
+	public void teste() {
+		
+	}
+}
--- src/test/java/com/uniritter/instaRitterTeam/services/ListarFotoTest.java
+++ src/test/java/com/uniritter/instaRitterTeam/services/ListarFotoTest.java
@@ -1,33 +1,27 @@
 package com.uniritter.instaRitterTeam.services;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-import java.security.InvalidParameterException;
 import java.util.Calendar;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import com.uniritter.instaRitterTeam.BaseTeste;
 import com.uniritter.instaRitterTeam.dtos.FotoParaIncluirDto;
 import com.uniritter.instaRitterTeam.models.Foto;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@TestPropertySource("classpath:test.properties")
-public class ConsultarFotoTest {
+public class ListarFotoTest extends BaseTeste {
 
 	@Autowired
 	@Qualifier("fotoServiceDatabase")
 	FotoService fotoService;
 
 	@Test
-	public void deveConsultarFoto() {
+	public void deveListarFotos() {
 		Byte[] conteudo = new Byte[3];
 		conteudo[0] = new Byte("1");
 		conteudo[1] = new Byte("2");
@@ -35,17 +29,32 @@ public class ConsultarFotoTest {
 		String legenda = "legenda";
 		Calendar dataAtual = Calendar.getInstance();
 		FotoParaIncluirDto dto = new FotoParaIncluirDto(legenda, conteudo);
-		Foto foto = fotoService.incluir(dto);
+		Foto foto1 = fotoService.incluir(dto);
 
-		Foto fotoIncluida = fotoService.consultar(foto.getId());
+		Byte[] conteudo2 = new Byte[3];
+		conteudo2[0] = new Byte("4");
+		conteudo2[1] = new Byte("6");
+		conteudo2[2] = new Byte("1");
+		String legenda2 = "legenda2";
+		Calendar dataAtual2 = Calendar.getInstance();
+		FotoParaIncluirDto dto2 = new FotoParaIncluirDto(legenda2, conteudo2);
+		Foto foto2 = fotoService.incluir(dto2);
 
-		Assert.assertEquals(fotoIncluida.getLegenda(), legenda);
-		Assert.assertArrayEquals(fotoIncluida.getConteudo(), conteudo);
-		Assert.assertEquals(dataAtual.get(Calendar.DAY_OF_YEAR), fotoIncluida.getData().get(Calendar.DAY_OF_YEAR));
+		List<Foto> fotos = fotoService.listar();
+
+		Assert.assertEquals(fotos.get(0).getId(), foto1.getId());
+		Assert.assertEquals(fotos.get(0).getLegenda(), foto1.getLegenda());
+		Assert.assertArrayEquals(fotos.get(0).getConteudo(), foto1.getConteudo());
+		Assert.assertEquals(dataAtual.get(Calendar.DAY_OF_YEAR), fotos.get(0).getData().get(Calendar.DAY_OF_YEAR));
+
+		Assert.assertEquals(fotos.get(1).getId(), foto2.getId());
+		Assert.assertEquals(fotos.get(1).getLegenda(), foto2.getLegenda());
+		Assert.assertArrayEquals(fotos.get(1).getConteudo(), foto2.getConteudo());
+		Assert.assertEquals(dataAtual.get(Calendar.DAY_OF_YEAR), fotos.get(1).getData().get(Calendar.DAY_OF_YEAR));
 	}
 
 	@Test
-	public void deveRetornarNullQuandoFotoNaoExistir() {
-		assertNull(fotoService.consultar(999l));
+	public void deveRetornarUmaListaVaziaQuandoNaoExistirFotosCadastradas() {
+		assertTrue(fotoService.listar().isEmpty());
 	}
 }
