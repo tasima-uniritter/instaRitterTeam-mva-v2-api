5f321872e41bdf6d294c79df30ce4713e3d906da
Gian Regis Frick <grfrick@stefanini.com>
2018-07-27 02:22:41 -0300

    adicionado swagger

--- src/main/java/com/uniritter/instaRitterTeam/InstaRitterTeamMvaV2ApiApplication.java
+++ src/main/java/com/uniritter/instaRitterTeam/InstaRitterTeamMvaV2ApiApplication.java
@@ -1,9 +1,13 @@
 package com.uniritter.instaRitterTeam;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class InstaRitterTeamMvaV2ApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/uniritter/instaRitterTeam/controllers/FotoController.java
+++ src/main/java/com/uniritter/instaRitterTeam/controllers/FotoController.java
@@ -13,22 +13,45 @@ import com.uniritter.instaRitterTeam.dtos.FotoParaIncluirDto;
 import com.uniritter.instaRitterTeam.models.Foto;
 import com.uniritter.instaRitterTeam.services.FotoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
+@Api(value = "Foto")
 public class FotoController {
 
 	@Autowired
 	private FotoService service;
 
+	@ApiOperation(value = "Incluir Fotos", response = Foto.class)
 	@RequestMapping(value = "/fotos", method = RequestMethod.PUT)
 	public Foto incluir(@RequestBody FotoParaIncluirDto fotoParaIncluirDto) {
 		return service.incluir(fotoParaIncluirDto);
 	}
 
+	@ApiOperation(value = "consultar foto", response = Foto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
 	@RequestMapping(value = "/fotos/{id}", method = RequestMethod.GET)
 	public Foto consultar(@PathVariable("id") Long id) {
 		return service.consultar(id);
 	}
 
+	@ApiOperation(value = "listar fotos", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
 	@RequestMapping(value = "/fotos", method = RequestMethod.GET)
 	public List<Foto> listar() {
 		return service.listar();
